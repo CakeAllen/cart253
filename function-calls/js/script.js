27f8a74c1316ebd867dcd4576439d6ed9487415b
Daniel <157847181+CakeAllen@users.noreply.github.com>
2025-09-09 14:09:02 -0400

    Basic commenting
    Added the appropriate comments to the js file.

--- function-calls/js/script.js
+++ function-calls/js/script.js
@@ -1,20 +1,32 @@
 /**
- * Title of Project
- * Author Name
+ * Function Calls
+ * Daniel Michurov
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * This is supposed to draw an image that ressembles a teal frame using p5.
+ * 
+ * Uses : 
+ * p5.js
+ * https://p5js.org/
  */
 
 "use strict";
 
+/**
+ * Creates a 640x480 canvas on startup.
+ */
 function setup() 
 {
+	// Create the canvas at a set resolution.
 	createCanvas(640, 480);
 }
 
+/**
+ * Draws a teal background and a white rectangle every frame.
+ */
 function draw() 
 {
+	// The teal background.
 	background(25, 100, 125);
+	// White rectangle.
 	rect(50, 50, 540, 380);
 }
