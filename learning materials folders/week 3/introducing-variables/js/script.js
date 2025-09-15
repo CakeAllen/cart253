c75fa2c7e58ff503e4f7f4ce68e20f1684b2b03e
Daniel <rayxray@live.ca>
2025-09-15 11:26:50 -0400

    introducing variables

---
+++ learning materials folders/week 3/introducing-variables/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Introducing Variables
+ * Daniel Michurov
+ * 
+ * 
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
