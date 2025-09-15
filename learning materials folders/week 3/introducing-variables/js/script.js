152f61a727410e3efce073c6bb9f8207dec834eb
Daniel <rayxray@live.ca>
2025-09-15 12:30:55 -0400

    introducing variables lesson 1
    finished the first lesson

--- learning materials folders/week 3/introducing-variables/js/script.js
+++ learning materials folders/week 3/introducing-variables/js/script.js
@@ -2,22 +2,32 @@
  * Introducing Variables
  * Daniel Michurov
  * 
- * 
+ * This allows for a circle to be drawn in the centre of the screen and changes colour based on the mouse's position.
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Canvas creation on setup
 */
-function setup() {
-
+function setup()
+{
+    // creates canvas
+    createCanvas(640, 640);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Drawing a circle.
 */
-function draw() {
-
+function draw()
+{
+    // draws a circle in the centre whose colour changes based on mouse pos
+    push();
+    noStroke();
+    // the red and green values change based on the mouse's X and Y coordinates respectively. the blue value stays at 0.
+    fill(mouseX, mouseY, 0);
+    // the ellipse's position will be at the very centre of the canvas.
+    ellipse(width / 2, height / 2, 100, 100);
+    pop();
 }
